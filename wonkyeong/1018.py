f4234194d950627a15cddef332b93d5d6c776959
JoyceWLee <50645935+JoyceWLee@users.noreply.github.com>
2021-05-06 21:07:11 +0900

    problem 1018

---
+++ wonkyeong/1018.py
@@ -0,0 +1,21 @@
+import sys
+
+N, M = map(int, (sys.stdin.readline().split()))
+cnt = 0
+
+for r in range(N):
+    row = list(sys.stdin.readline().split()[0])
+    for c in range(M):
+        if r == 0:
+            if row[0] == 'B':
+                start0, start1 = 'B', 'W'
+            else:
+                start0, start1 = 'W', 'B'
+        if (r % 2 == c % 2):
+            if row[c] != start0:
+                cnt += 1
+        else:
+            if row[c] != start1:
+                cnt += 1
+
+print(cnt)
