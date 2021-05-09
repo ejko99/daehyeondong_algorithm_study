a17f47dacc27879215e161e7a391aa09ceb5db58
JoyceWLee <ewonkyong@naver.com>
2021-05-09 14:32:23 +0900

    problem 1018

--- wonkyeong/1018.py
+++ wonkyeong/1018.py
@@ -5,6 +5,7 @@ cnt = 0
 
 for r in range(N):
     row = list(sys.stdin.readline().split()[0])
+    print(row)
     for c in range(M):
         if r == 0:
             if row[0] == 'B':
@@ -13,9 +14,12 @@ for r in range(N):
                 start0, start1 = 'W', 'B'
         if (r % 2 == c % 2):
             if row[c] != start0:
+                row[r][c] = start0
                 cnt += 1
         else:
             if row[c] != start1:
+                row[r][c] = start1
                 cnt += 1
+    print(row)
 
 print(cnt)
