31dc3a366c6153568029b180a7535c1112cfd580
JoyceWLee <50645935+JoyceWLee@users.noreply.github.com>
2021-05-18 23:51:22 +0900

    2108.py

--- wonkyeong/2108.py
+++ wonkyeong/2108.py
@@ -2,11 +2,12 @@ import sys
 
 n = int(sys.stdin.readline())
 l = [int(sys.stdin.readline()) for _ in range(n)]
-l.sort()
-maxcnt = n
+
+for i in range(n-1):
+    for j in range(n-1-i):
+        if l[j] > l[j+1]:
+            l[j], l[j+1] = l[j+1], l[j]
 
 print(int(sum(l)/n))
-print(l[(n//2) + 1])
-for i in l:
-    maxcnt = max(maxcnt, l.count(i))
-print()
+print(l[int((n-1)/2)])
+print(l[-1]-l[0])
