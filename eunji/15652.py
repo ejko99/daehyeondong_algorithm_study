dc2ea06cec4bb6e9a2c77c270c83648b86cbfd6f
kole2706 <kosu7071@naver.com>
2021-05-20 20:22:56 +0900

    backtracking

---
+++ eunji/15652.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+n,m = map(int,input().split())
+
+result = [0]*10
+
+def promising(result,i):
+    switch = True
+    for k in range(i):
+        if result[i-1] > result[i]:
+            switch = False
+            break
+    return switch
+
+def nandm(i):
+    if promising(result,i):
+        if i==m:
+            print(*[result[k] for k in range(1,m+1)])
+        else:
+            for j in range(1,n+1):
+                result[i+1] = j
+                nandm(i+1)
+
+nandm(0)
