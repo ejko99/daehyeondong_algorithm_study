989e762894d863043d27381ee32cefc55bbfcdb8
JoyceWLee <50645935+JoyceWLee@users.noreply.github.com>
2021-05-22 23:49:56 +0900

    15650.py

---
+++ wonkyeong/15650.py
@@ -0,0 +1,23 @@
+import sys
+
+m, n = map(int, sys.stdin.readline().split())
+ans = []
+visited = [False for _ in range(m)]
+
+
+def backtracking(depth, n, m):
+
+    if depth == n:
+        print(" ".join(map(str, ans)))
+        return
+    
+    for i in range(1, m+1):
+        #print(ans)
+        if not visited[i-1]:
+            visited[i-1] = True
+            ans.append(i)
+            backtracking(depth+1, n, m)
+            visited[i-1] = False
+            ans.pop()
+
+backtracking(0, n, m)
