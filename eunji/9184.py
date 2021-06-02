36b7de4c6cd895f7a804fcf3355d1854d9dfad84
kole2706 <kosu7071@naver.com>
2021-06-02 23:53:34 +0900

    0602

--- eunji/9184.py
+++ eunji/9184.py
@@ -6,7 +6,10 @@ arr = [[[0]*20]*20]*20
 print(arr)
 
 
+
 while(1):
     nums = list(map(int,input().split()))
     if nums == [-1,-1,-1]:
         break
+    if nums[0]>20 or nums[1]>20 or nums[2]>20:
+        print(arr[20,20,20])
