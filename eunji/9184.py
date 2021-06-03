b37219067c204e0e024602d438e7bfb412d8cdc6
kole2706 <kosu7071@naver.com>
2021-06-03 17:06:16 +0900

    0603

--- eunji/9184.py
+++ eunji/9184.py
@@ -13,3 +13,7 @@ while(1):
         break
     if nums[0]>20 or nums[1]>20 or nums[2]>20:
         print(arr[20,20,20])
+    if nums[0]<=0 or nums[1]<=0 or nums[2]<=0:
+        print(1)
+    else:
+        print(arr[nums[0],nums[1],nums[2]])
